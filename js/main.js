9d915669255d48a41f5b46721d88920fbc313e19
okmanl <ericgumba@gmail.com>
2019-11-25 20:59:06 -0800

    added js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("AOSDLAOSKDOFKAS")
+let allMessages = document.getElementById('allMessages')
+
+
+let sendMessage = document.getElementById('sendMessage')
+
+let message = document.getElementById('userMessage')
+
+
+let getAllMessages = async function(){
+    let url = 'https://k887e1wr7b.execute-api.us-west-2.amazonaws.com/prod/message'
+    let response = await fetch(url) 
+    messages =  await response.json() 
+
+    console.log(messages)
+}
+ 
+
+console.log("HELLO ")
+getAllMessages()
